122cd33eee8c926fc19ca56d7935ae5d430f236a
Alexander Kuryatnikov <asander1995@gmail.com>
2023-01-28 17:19:34 +0300

    leetcode problem 3

---
+++ 3_longest_substring_without_repeating_characters/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+
+        left = 0
+        right = 0
+        sub_char_set = {s[0]}
+
+        sub_len = 1
+        max_sub_len = 1
+
+        while right < len(s) - 1:
+            right += 1
+
+            if s[right] in sub_char_set:
+                while s[left] != s[right]:
+                    sub_char_set.remove(s[left])
+                    left += 1
+                left += 1
+            else:
+                sub_char_set.add(s[right])
+
+            sub_len = right - left + 1
+            if sub_len > max_sub_len:
+                max_sub_len = sub_len
+
+        return max_sub_len
+
+
+solution = Solution()
---
+++ 3_longest_substring_without_repeating_characters/test.py
@@ -0,0 +1,52 @@
+from solution import solution
+
+
+TEST_VALUES = {
+    '1: empty string': [
+        '',
+        0
+    ],
+    '2: one char': [
+        'a',
+        1
+    ],
+    '3: two same chars': [
+        'aa',
+        1
+    ],
+    '4: long string of same char': [
+        'aaaaaaaaa',
+        1
+    ],
+    '5: longest substring is string itself': [
+        'abcdefg',
+        7
+    ],
+    '6: longest substring first': [
+        'ab1cde1f',
+        6
+    ],
+    '7: longest substring last': [
+        'a1b1cde',
+        5
+    ],
+    '8: logest substring in the middle': [
+        '1a1bcdefg1h1',
+        8
+    ],
+    '9: substrings intersect with first longer': [
+        'a1bc2de1f2g',
+        7
+    ],
+    '10: substrings intersect with second longer': [
+        'a1bc2d1efg2h',
+        8
+    ],
+}
+
+
+def test():
+    for test, values in TEST_VALUES.items():
+        result = solution.lengthOfLongestSubstring(values[0])
+        assert values[1] == result, f'Test {test}. Your result {result}'
+        print(f'Test {test}... OK')
