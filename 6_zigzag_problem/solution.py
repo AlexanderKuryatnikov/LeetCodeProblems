a4d7c81e2d9bde47fcbbab3e3be553640f86a0e2
Alexander Kuryatnikov <asander1995@gmail.com>
2023-01-30 20:57:41 +0300

    problem 6

---
+++ 6_zigzag_problem/solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        s_len = len(s)
+        new_s = [' '] * s_len
+        j = 0
+
+        i = 0
+        while i < s_len:
+            new_s[j] = s[i]
+            j += 1
+            i += 2 * numRows - 2
+
+        for row in range(1, numRows - 1):
+            i = row
+            while i < s_len:
+                new_s[j] = s[i]
+                j += 1
+                try:
+                    new_s[j] = s[i + 2 * (numRows - row - 1)]
+                    j += 1
+                except IndexError:
+                    break
+                i += 2 * numRows - 2
+
+        i = numRows - 1
+        while i < s_len:
+            new_s[j] = s[i]
+            j += 1
+            i += 2 * numRows - 2
+
+        return ''.join(new_s)
+
+
+solution = Solution()
---
+++ 6_zigzag_problem/test.py
@@ -0,0 +1,47 @@
+from solution import solution
+
+
+TEST_VALUES = {
+    '1: single row': [
+        'abcd',
+        1,
+        'abcd'
+    ],
+    '2: leetcod example 1': [
+        'PAYPALISHIRING',
+        3,
+        'PAHNAPLSIIGYIR'
+    ],
+    '3: leetcode example 2': [
+        'PAYPALISHIRING',
+        4,
+        'PINALSIGYAHRPI'
+    ],
+    '4: index error': [
+        'abcdefghijklmnop',
+        5,
+        'aibhjpcgkodflnem'
+    ],
+    '5: two rows 1': [
+        'abcdefg',
+        2,
+        'acegbdf'
+    ],
+    '6: two rows 2': [
+        'abcdefgh',
+        2,
+        'acegbdfh'
+    ],
+    '7: several rows with single char': [
+        'a',
+        3,
+        'a'
+    ],
+}
+
+
+def test():
+    for test, values in TEST_VALUES.items():
+        result = solution.convert(values[0], values[1])
+        assert values[2] == result, f'Test {test}. Your result {result}'
+        print(f'Test {test}... OK')
